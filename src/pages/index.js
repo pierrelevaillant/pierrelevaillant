04eba055a817b67585f028b747aeec165c89cd51
Pierre Le Vaillant <pierrelevaillant@pierres-imac.home>
2019-03-26 11:57:22 +0100

    Setup: Remove react-reveal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Slide from 'react-reveal/Slide';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,14 +7,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Pierre Le Vaillant – Product designer" keywords={[`portfolio`, `interactive designer`, `product designer`]} />
     <div style={{ overflow: `hidden`, }}>
-      <Slide bottom>
-        <h1>Pierre Le Vaillant</h1>
-      </Slide>
+      <h1>Pierre Le Vaillant</h1>
     </div>
     <div style={{ overflow: `hidden`, }}>
-      <Slide bottom>
-        <h2>Designer, <a href="https://www.area17.com" target="_blank" rel="noopener noreferrer">AREA 17</a></h2>
-      </Slide>
+      <h2>Designer, <a href="https://www.area17.com" target="_blank" rel="noopener noreferrer">AREA 17</a></h2>e>
     </div>
   </Layout>
 )
