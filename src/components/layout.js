24ceec5d08e328b4781ad1f5eaffa2e800851c45
Pierre Le Vaillant <pierrelevaillant@pierres-imac.home>
2019-03-26 11:40:11 +0100

    Setup: Initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => (
@@ -25,21 +24,15 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
+      <div style={{
+          padding: `2vw`,
+          height: `100vh`,
+        }}
         >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}
+        </footer>
         </div>
       </>
     )}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,22 @@
 import React from "react"
-import { Link } from "gatsby"
+import Slide from 'react-reveal/Slide';
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO title="Pierre Le Vaillant – Product designer" keywords={[`portfolio`, `interactive designer`, `product designer`]} />
+    <div style={{ overflow: `hidden`, }}>
+      <Slide bottom>
+        <h1>Pierre Le Vaillant</h1>
+      </Slide>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div style={{ overflow: `hidden`, }}>
+      <Slide bottom>
+        <h2>Designer, <a href="https://www.area17.com" target="_blank" rel="noopener noreferrer">AREA 17</a></h2>
+      </Slide>
+    </div>
   </Layout>
 )
 
